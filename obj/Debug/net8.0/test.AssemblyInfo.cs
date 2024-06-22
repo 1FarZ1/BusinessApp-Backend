184dd80f18f7046cc0aef7793bc88cba87719bf4
1FarZ1 <fbekkouche149@gmail.com>
2024-06-22 01:00:30 +0100

    chore: Remove AuthorizationHeaderParameterOperationFilter

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c01ba6355f24cc9abba4a2de16b31999bbb010f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ebe3fb37a2a4eb4cc778acf9b5ac3cc8cf244c")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
